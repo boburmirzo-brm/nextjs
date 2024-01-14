b6b63f7354c6e5af9385675bd446de638c748928
boburmirzo-brm <boburmirzo.brm@gmail.com>
2024-01-14 16:00:40 +0500

    change cors

--- next.config.js
+++ next.config.js
@@ -1,33 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        // Routes this applies to
-        source: "/api/(.*)",
-        // Headers
-        headers: [
-          // Allow for specific domains to have access or * for all
-          {
-            key: "Access-Control-Allow-Origin",
-            // value: "*",
-            // DOES NOT WORK
-            value: "https://nextjs-dusky-iota-92.vercel.app",
-          },
-          // Allows for specific methods accepted
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          // Allows for specific headers accepted (These are a few standard ones)
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
-};
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
